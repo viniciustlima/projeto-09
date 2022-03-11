9c884e998f19e1362b0bed21c5b4ec89a839eeef
viniciustlima <vinitlima2007.14@gmail.com>
2022-03-11 19:51:54 -0300

    Code refactor.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
-const app = require("./app/app");
+require('dotenv').config()
+const app = require('./app/app')
 
-var PORT = process.env.PORT || 3333;
+var PORT = process.env.PORT || 3333
 
 app.listen(PORT, () => {
   console.log(
     `=====================================\n|  Server listening on port: ${PORT}   |\n=====================================`
-  );
-});
+  )
+})
