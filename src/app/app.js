8acd212da422ea3976f736c9343151eee3e12807
viniciustlima <vinitlima2007.14@gmail.com>
2022-03-10 23:35:13 -0300

    ExpressSession initialized and html set as template engine

--- src/app/app.js
+++ src/app/app.js
@@ -1,38 +1,46 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
-const ExpressSession = require("express-session");
+const session = require('express-session')
 
-const routes = require("./routes/routes.js");
-const path = require("path");
+const routes = require('./routes/routes.js')
+const path = require('path')
 
 class App {
   constructor() {
-    this.app = express();
-    this.middlewares();
-    this.routes();
+    this.app = express()
+    this.middlewares()
+    this.routes()
   }
 
   middlewares() {
-    this.app.engine("ejs", require("ejs").renderFile);
-    this.app.set("view engine", "ejs");
-    this.app.set("views", path.join(__dirname, "../views/pages"));
-    this.app.use(express.static(path.join(__dirname, "../views/public")));
-    this.app.use(morgan("dev"));
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({ extended: true }))
+    this.app.use(session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true,
+    }))
+    this.app.set("trust proxy", 1);
+    this.app.engine('html', require('ejs').renderFile)
+    this.app.set('view engine', 'html')
+    this.app.set('views', path.join(__dirname, '../views/pages'))
+    this.app.use(express.static(path.join(__dirname, '../views/public')))
+    this.app.use(morgan('dev'))
     this.app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-      res.header("Access-Control-Allow-Headers","Access, Content-Type, Authorization, Acept, Origin, X-Requested-With");
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+      res.header('Access-Control-Allow-Headers','Access, Content-Type, Authorization, Acept, Origin, X-Requested-With')
 
       this.app.use(cors())
-      next();
-    });
+      next()
+    })
   }
 
   routes() {
-    this.app.use(routes);
+    this.app.use(routes)
   }
 }
 
-module.exports = new App().app;
+module.exports = new App().app
