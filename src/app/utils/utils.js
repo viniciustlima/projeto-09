2015615271c7ce8144bc62679fbda78dad332c52
viniciustlima <vinitlima2007.14@gmail.com>
2022-03-12 20:18:07 -0300

    Add role functions.

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -1,7 +1,37 @@
 class Utils {
 	percentage = (p, t) => {
-    return (p * 100) / t;
+    return (p * 100) / t
+  }
+
+  getRole = (r) => {
+    const roles = [
+      { roleId: 0, roleName: "Administrador" },
+      { roleId: 1, roleName: "Moderador" },
+      { roleId: 2, roleName: "Usuário Comum" } 
+    ]
+
+    for (let i = 0; i < roles.length; i++) {
+      if (r === roles[i].roleId) {
+        return roles[i].roleName
+        break
+      }
+    }
+  }
+
+  getRoleIcon = (r) => {
+    const roles = [
+      { roleId: 0, roleIcon: "fas fa-star" },
+      { roleId: 1, roleIcon: "fas fa-crown" },
+      { roleId: 2, roleIcon: "fas fa-user" }
+    ]
+
+    for (let i = 0; i < roles.length; i++) {
+      if (r === roles[i].roleId) {
+        return roles[i].roleIcon
+        break
+      }
+    }
   }
 }
 
-module.exports = new Utils();
+module.exports = new Utils()
