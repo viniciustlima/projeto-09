12b5c0315ae50bd466fd90a3912a4b48836d0554
viniciustlima <vinitlima2007.14@gmail.com>
2022-03-15 22:59:32 -0300

    Change roles id's and code refactor.

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -1,36 +1,26 @@
 class Utils {
-	percentage = (p, t) => {
-		return (p * 100) / t
-	}
+	percentage = (p, t) => (p * 100) / t
 
 	getRole = (r) => {
 		const roles = [
-			{ roleId: 0, roleName: 'Administrador' },
+			{ roleId: 2, roleName: 'Administrador' },
 			{ roleId: 1, roleName: 'Moderador' },
-			{ roleId: 2, roleName: 'Usuário Comum' },
+			{ roleId: 0, roleName: 'Usuário Comum' },
 		]
 
-		for (let i = 0; i < roles.length; i++) {
-			if (r === roles[i].roleId) {
-				return roles[i].roleName
-				break
-			}
-		}
+		for (let i = 0; i < roles.length; i++)
+			if (r === roles[i].roleId) return roles[i].roleName
 	}
 
 	getRoleIcon = (r) => {
 		const roles = [
-			{ roleId: 0, roleIcon: 'fas fa-star' },
+			{ roleId: 2, roleIcon: 'fas fa-star' },
 			{ roleId: 1, roleIcon: 'fas fa-crown' },
-			{ roleId: 2, roleIcon: 'fas fa-user' },
+			{ roleId: 0, roleIcon: 'fas fa-user' },
 		]
 
-		for (let i = 0; i < roles.length; i++) {
-			if (r === roles[i].roleId) {
-				return roles[i].roleIcon
-				break
-			}
-		}
+		for (let i = 0; i < roles.length; i++)
+			if (r === roles[i].roleId) return roles[i].roleIcon
 	}
 }
 
