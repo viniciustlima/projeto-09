4ddc830f64d3669cc9d38d7e83f514a0bf536d06
viniciustlima <vinitlima2007.14@gmail.com>
2022-03-15 23:16:40 -0300

    Change log message.

--- src/server.js
+++ src/server.js
@@ -4,7 +4,5 @@ const app = require('./app/app')
 var PORT = process.env.PORT || 3333
 
 app.listen(PORT, () => {
-	console.log(
-		`=====================================\n|  Server listening on port: ${PORT}   |\n=====================================`
-	)
+	console.log(`http://localhost:${PORT}/`)
 })
