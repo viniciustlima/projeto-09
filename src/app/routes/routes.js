676b27f0662aae3622a6833d2545d754de57f390
viniciustlima <vinitlima2007.14@gmail.com>
2022-03-15 23:19:52 -0300

    Add middleware, '/add-user' route and global ('*') route with redirect to home ('/').

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 
 const home = require('../controllers/home.controller')
 
+const loginmid = require('../middlewares/login.middleware')
+
 router.get('/', home.index)
 router.post('/', home.post)
 
@@ -9,5 +11,15 @@ router.get('/logout', (req, res) => {
 	req.session.login = undefined
 	return res.redirect('/')
 })
+
+router.get('/add-user', loginmid, (req, res) => {
+	return res.render('home', {
+		login: req.session.login,
+		utils: require('../utils/utils'),
+		page: req.originalUrl,
+	})
+})
+
+router.get('*', loginmid, (req, res) => res.redirect('/'))
 
 module.exports = router
